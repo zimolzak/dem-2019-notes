07a785ce59f2b25a8830c4e47748a708c56afc17
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-11-12 14:16:50 -0500

    Remove some punctuation before word counting.

--- split.py
+++ split.py
@@ -2,6 +2,8 @@
 
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
+punctuation = '- . , * : ; ? ( ) " ='.split()
+
 # https://gist.github.com/sebleier/554280
 # NLTK's list of english stopwords
 
@@ -9,6 +11,8 @@ from sys import argv
 with open(argv[1]) as fh:
     for L in fh:
         L = L.lower()
+        for p in punctuation:
+            L = L.replace(p, '')
         wlist = L.split()
         for w in wlist:
             if w not in stopwords:
